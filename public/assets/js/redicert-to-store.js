c9a3c57948c6a84d8b5a979efb1254b424a74762
asgaraliyev <asgaraliyev2001@gmail.com>
2022-05-10 16:39:35 +0400

    1 binaries uploaded

--- public/assets/js/redicert-to-store.js
+++
@@ -1,9 +0,0 @@
-
-let OS = getOS()
-if (OS === "ios") {
-    window.location.replace(window.state.downloadLinks.ios)
-} else if (OS === "android") {
-    window.location.replace(window.state.downloadLinks.android)
-} else {
-    window.location.replace(window.state.downloadLinks.android)
-}
